3e42b79e5f553db161a4e4433586621360314def
deya taupier <30326911+miscdats@users.noreply.github.com>
2019-02-18 00:49:55 -0800

    upload files
    Complete instructions with solutions~

---
+++ introduction.js
@@ -0,0 +1,28 @@
+// ## INTRODUCTION (Exercise 1 of 19)
+//
+//   To keep things organized, let's create a folder for this workshop.
+//
+//   Run this command to make a directory called javascripting (or something
+//   else if you like):
+//
+//      mkdir javascripting
+//
+//   Change directory into the javascripting folder:
+//
+//      cd javascripting
+//
+//   Create a file named introduction.js:
+//
+//      touch introduction.js
+//
+//   Or if you're on Windows:
+//
+//      type NUL > introduction.js
+//
+//   (type is part of the command!)
+//
+//   Open the file in your favorite editor, and add this text:
+//
+//      console.log('hello');
+
+console.log('hello');
---
+++ strings.js
@@ -0,0 +1,13 @@
+// The challenge:
+//
+//  For this challenge, create a file named strings.js.
+//
+//  In that file create a variable named someString like this:
+//
+//     var someString = 'this is a string';
+//
+//  Use console.log to print the variable someString to the terminal.
+
+let someString  = 'this is a string';
+
+console.log(someString);
